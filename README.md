bd2b615772367b9168a29fd1a4ad8323757f5805
graell <gresyaleman12@gmail.com>
2021-08-15 23:07:44 +0700

    update repo format

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ Diciptakan oleh Kelompok 4 - Epimilis
 ## Anggota (Kelompok 4 - Epimilis)
 | Nama               | NIM     | Job Desc  |
 |--------------------|---------|-----------|
-| Gresya Angelina Eunike Leman | 16520082 | Insert Job Here |
-| Frederik Imanuel Louis | 16520103 | Insert Job Here |
-| Andreana Hartadi Suliman | 16520113 | Insert Job Here |
-| Wesly Giovano | 16520363 | Insert Job Here |
-| M Syahrul Surya Putra | 16520430 | Insert Job Here |
-| David Nathanio Gabriel Siahaan | 16520432 | Insert Job Here |
-| Annel Rashka Perdana | 16520448 | Insert Job Here |
-| Afkar Dhiya Ulhaq | 16520452 | Insert Job Here |
-| Rifki Kaida | 16520496 | Insert Job Here |
+| Gresya Angelina Eunike Leman | 16520082 | Desain Karakter, Backend Utility (Command Help) |
+| Frederik Imanuel Louis | 16520103 | Backend Main |
+| Andreana Hartadi Suliman | 16520113 | Desain Karakter, Backend Utility (Error Message) |
+| Wesly Giovano | 16520363 | Deploy Heroku, Bot (OA) Set Up |
+| M Syahrul Surya Putra | 16520430 | Backend Utility (Validasi + Prefix) |
+| David Nathanio Gabriel Siahaan | 16520432 | Backend Utility (Command Creator) |
+| Annel Rashka Perdana | 16520448 | Backend Main |
+| Afkar Dhiya Ulhaq | 16520452 | Backend Utility (QA) |
+| Rifki Kaida | 16520496 | Backend Utility (Welcome Message) |
 
 ## Cara Menggunakan
 
---
+++ src/line-api.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, FollowEvent, TextSendMessage,
+)
+
+import backend
+
+
+app = Flask(__name__)
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = 'ecc29561200057475cbdca1668d6b7da'
+channel_access_token = 'F1VRU2c3omjPFzbW5rPqvVWkgGBhJf3ZDux8HNJIpYgdJAdZuzHpSm+gJjLndjpKyAQhPAf97JrSymWug97r2kji/jM2GIeCTC9iEf7LAhg8W6CyRHZMCtDLBD4s92hP7qJVnSO7HQKE7jVC1qrasgdB04t89/1O/w1cDnyilFU='
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['x-line-signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent)
+def check_message(event):
+    if event.message.type == 'text':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=backend.hoax(event.message.text))
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Sorry, currently we can only handle text messages.')
+        )
+
+
+@handler.add(FollowEvent)
+def greeting_message(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    greeting_text = 'Hi, ' + profile.display_name + '!\n' + \
+                    'Thank you for adding Selekta : Seleksi Fakta, your friend in finding the truth.\n'
+    line_bot_api.push_message(
+        event.source.user_id,
+        TextSendMessage(text=greeting_text)
+    )
+
+
+if __name__ == "__main__":
+    app.run()
